85593b9a5b07860e6674395eb31908cbb74c1fed
1997LP <1950517655@qq.com>
2020-03-24 13:31:04 +0800

    timer窗体

---
+++ timer/timer/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.开始 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(49, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // 开始
+            // 
+            this.开始.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.开始.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.开始.Location = new System.Drawing.Point(374, 51);
+            this.开始.Name = "开始";
+            this.开始.Size = new System.Drawing.Size(151, 66);
+            this.开始.TabIndex = 1;
+            this.开始.Text = "开始";
+            this.开始.UseVisualStyleBackColor = false;
+            this.开始.Click += new System.EventHandler(this.开始_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(49, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(52, 151);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(473, 35);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(122, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(131, 23);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(119, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(568, 216);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.开始);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "定时器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button 开始;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ timer/timer/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer
+{
+    public partial class Form1 : Form
+    {
+        int p_count = 0;    //定时器到一次，加1
+        int p_timer = 0;    //记录定时时间
+        int p_ret = 0;      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "时长";
+            label2.Text = "剩余时长";
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;  //设置文本框不可编辑，只能下拉选择
+
+            for (int i=0;i<101;i++)
+            {
+                comboBox1.Items.Add(i.ToString()+" 秒");  //添加下拉框选项
+            }
+
+            label3.Text = "0 秒";
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 开始_Click(object sender, EventArgs e)
+        {
+            System.Media.SystemSounds.Beep.Play();  //系统提示音
+            string timer = comboBox1.Text;
+            if (timer == "")    //判断下拉框是否为空，即是否选择了定时时间
+            {
+                MessageBox.Show("请选择定时时间");
+            }
+            else
+            {
+                string data = timer.Substring(0, 2);  //读取字符串的前两个字符
+
+                p_timer = p_ret = Convert.ToInt16(data);    //将前两个字符转换为int16的类型
+                p_count = 0;
+
+                progressBar1.Value = progressBar1.Minimum;  //设置进度条的初始值
+                progressBar1.Maximum = p_timer;             //设置进度条的最大值
+                progressBar1.Minimum = 0;                   //设置进度条的最小值
+
+                label3.Text = comboBox1.Text;
+
+                timer1.Start();         //打开定时器
+
+            }
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)//定时器处理函数
+        {
+            p_count++;
+            p_ret--;
+            if (p_count >= p_timer)  //如果定时时间到
+            {
+                progressBar1.Value = progressBar1.Maximum;  //设置进度条为最大值
+                label3.Text = "0 秒";
+                timer1.Stop();      //关闭定时器
+                System.Media.SystemSounds.Asterisk.Play();//系统提示音
+            
+                MessageBox.Show("定时时间到"); //它会使程序阻塞，所以应该写在后面
+
+            }
+            if (p_count < p_timer)  //定时时间没有到
+            {
+                progressBar1.Value = p_count;  //修改进度条
+                label3.Text = p_ret.ToString() + " 秒"; //显示剩余时间
+            }
+           
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
